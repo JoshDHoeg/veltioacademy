8954e1929ed68f0a8433b988259712d4d5816519
Josh Hoeg <jdhoeg97@gmail.com>
2017-07-01 11:36:38 -0600

    initial commit for veltio project

---
+++ env27/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\joshho~1\docume~1\github\veltio~1\va\env27\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
